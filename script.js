462f669e2831e763a0f5ef02411a9c2793631a8f
Makhosetive7 <sibandamakhosetive7@gmail.com>
2023-07-11 17:01:07 +0200

    11-07-2023

---
+++ script.js
@@ -0,0 +1,25 @@
+const VowelCountAndCheckPalendrome = () => {
+    const textarea = document.getElementById("textarea").value
+    const countVowels = Vowels(textarea)
+    const checkPalendrome = Palendrome(textarea)
+    const results = document.getElementById("results")
+
+    results.innerHTML = "Number of Vowels:" + countVowels + "and" + + "<br> isPalendrome" + checkPalendrome 
+}
+
+const Palendrome = (string)=> {
+    const reversedstring = string.split("").reverse().join("")
+    return string.toLowerCase() === reversedstring.toLowerCase()
+}
+
+const Vowels = (string) => {
+    let vowelscount=0;
+    let vowelsArray = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+
+    for (const char of string.toLowerCase()) {
+        if (vowelsArray.includes(char)) {
+            return vowelscount++
+        }
+    }
+    return vowelscount
+}
